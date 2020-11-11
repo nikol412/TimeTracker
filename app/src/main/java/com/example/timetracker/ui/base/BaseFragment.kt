bccb0fc8cf4a027aee69779fed7e35d3aa531cc0
nikol412 <travin.nikita@gmail.com>
2020-11-11 14:35:37 +0600

    implemented baseFragment

--- app/src/main/java/com/example/timetracker/ui/base/BaseFragment.kt
+++ app/src/main/java/com/example/timetracker/ui/base/BaseFragment.kt
@@ -5,18 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
 import dagger.android.support.DaggerFragment
 
-abstract class BaseFragment : DaggerFragment() {
+abstract class BaseFragment : Fragment() {
+
+    abstract fun baseViewModel(): BaseViewModel
 
     @LayoutRes
     abstract fun layoutRes(): Int
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(layoutRes(), container, false)
+    fun getBaseActivity(): BaseActivity = activity as BaseActivity
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        subscribeToViewModelsObservables()
+    }
+
+    protected open fun subscribeToViewModelsObservables() {
+        with(baseViewModel()) {
+            // TODO: implement this after implementing baseViewModel
+        }
+    }
+
+    protected fun showAlertDialogFragment(title: String? = null, message: String? = null) {
+        // TODO: implement this after implementing baseViewModel
     }
 }
