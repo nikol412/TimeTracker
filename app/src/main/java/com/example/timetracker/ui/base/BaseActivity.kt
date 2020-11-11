e53f7e69439dfe010f82f69436c5f8c8ebdf57ba
nikol412 <travin.nikita@gmail.com>
2020-11-11 14:23:21 +0600

    added baseActivity and configure bottom nav for mainActivity

--- app/src/main/java/com/example/timetracker/ui/base/BaseActivity.kt
+++ app/src/main/java/com/example/timetracker/ui/base/BaseActivity.kt
@@ -1,18 +1,30 @@
 package com.example.timetracker.ui.base
 
+import android.app.Activity
+import android.content.Intent
 import dagger.android.support.DaggerAppCompatActivity
 import android.os.Bundle
 import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
 
-abstract class BaseActivity : DaggerAppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity() {
 
     @LayoutRes
     abstract fun layoutRes(): Int
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layoutRes())
 
+        subscribeToViewModelObservables()
+    }
+
+    protected open fun subscribeToViewModelObservables() {
+
+    }
+
+    fun startActivity(activity: Class<out Activity>) {
+        val intent = Intent(this, activity)
+        startActivity(intent)
     }
 
 }
