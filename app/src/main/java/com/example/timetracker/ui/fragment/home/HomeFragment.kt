bca0a531efcbdbfed46643346f27d163a45dffdf
nikol412 <travin.nikita@gmail.com>
2020-11-26 16:44:18 +0600

    #5 add RecyclerView with test items and added layout for item row, added some icons

--- app/src/main/java/com/example/timetracker/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/example/timetracker/ui/fragment/home/HomeFragment.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import com.example.timetracker.R
 import com.example.timetracker.databinding.FragmentHomeBinding
 import com.example.timetracker.ui.base.BaseFragment
@@ -21,15 +19,19 @@ class HomeFragment : BaseFragment() {
     override fun layoutRes(): Int = R.layout.fragment_home
 
     lateinit var binding: FragmentHomeBinding
+    lateinit var adapter: ItemsAdapter
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, layoutRes(), container, false)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+        adapter = ItemsAdapter()
+        adapter.setItems(viewModel.items)
+        binding.homeRecyclerView.adapter = adapter
 
         return binding.root
     }
--- app/src/main/java/com/example/timetracker/ui/fragment/home/HomeViewModel.kt
+++ app/src/main/java/com/example/timetracker/ui/fragment/home/HomeViewModel.kt
@@ -5,7 +5,6 @@ import com.example.timetracker.App
 import com.example.timetracker.data.db.model.User
 import com.example.timetracker.data.db.repository.UserRepository
 import com.example.timetracker.ui.base.BaseViewModel
-import com.example.timetracker.ui.model.AlertObject
 import javax.inject.Inject
 
 class HomeViewModel : BaseViewModel() {
@@ -14,16 +13,34 @@ class HomeViewModel : BaseViewModel() {
     lateinit var userRepository: UserRepository
 
     var homeLabel = MutableLiveData<String>()
+    var items = mutableListOf(
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        19,
+        20
+    ).map { it.toString() }
 
     init {
         App.appComponent?.inject(this)
 
         userRepository.createUser(User(1))
         userRepository.getUserAsync()?.addChangeListener<User> { data, changeSet ->
-            onAlertDialogNeeded.value = AlertObject("Success", "User was created")
-            homeLabel.value = data.firstName + data.lastName + "cool".repeat(3)
         }
-
-
     }
 }
