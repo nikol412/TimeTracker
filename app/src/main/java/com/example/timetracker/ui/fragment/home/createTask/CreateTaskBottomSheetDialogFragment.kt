838b2ed10a2a15aa4b461751f6a30418f66bc3bf
nikol412 <travin.nikita@gmail.com>
2021-03-03 22:50:27 +0600

    #6 added design for bottom nav dialog

--- app/src/main/java/com/example/timetracker/ui/fragment/home/createTask/CreateTaskBottomSheetDialogFragment.kt
+++ app/src/main/java/com/example/timetracker/ui/fragment/home/createTask/CreateTaskBottomSheetDialogFragment.kt
@@ -1,5 +1,6 @@
 package com.example.timetracker.ui.fragment.home.createTask
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import androidx.fragment.app.viewModels
 import com.example.timetracker.R
 import com.example.timetracker.databinding.FragmentCreateTaskBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import java.util.*
 
 class CreateTaskBottomSheetDialogFragment : BottomSheetDialogFragment() {
 
@@ -17,6 +19,25 @@ class CreateTaskBottomSheetDialogFragment : BottomSheetDialogFragment() {
     fun layoutRes(): Int = R.layout.fragment_create_task
 
     lateinit var binding: FragmentCreateTaskBinding
+
+    private val datePicker by lazy {
+        val calendar = Calendar.getInstance()
+        val datePicker = DatePickerDialog(
+            requireContext(),
+            dateListener,
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        datePicker.datePicker.minDate = Calendar.getInstance().timeInMillis
+
+        return@lazy datePicker
+    }
+
+    val dateListener =
+        DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            viewModel.taskDate.value = "$dayOfMonth/$month/$year"
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,11 +48,24 @@ class CreateTaskBottomSheetDialogFragment : BottomSheetDialogFragment() {
         binding.vm = viewModel
         binding.lifecycleOwner = this
 
-        binding.buttonCreateTask.setOnClickListener {
-
-            this.dismiss()
-        }
+        subscribeViewModelVariables()
 
         return binding.root
     }
+
+    fun subscribeViewModelVariables() {
+        viewModel.event.observe(viewLifecycleOwner, { event ->
+            when (event) {
+                CreateTaskEvent.ADD_TASK -> this.dismiss()
+                CreateTaskEvent.DATE -> showDatePicker()
+            }
+        })
+    }
+
+
+    private fun showDatePicker() {
+        datePicker.show()
+    }
+
+
 }
--- app/src/main/java/com/example/timetracker/ui/fragment/home/createTask/CreateTaskBottomSheetDialogViewModel.kt
+++ app/src/main/java/com/example/timetracker/ui/fragment/home/createTask/CreateTaskBottomSheetDialogViewModel.kt
@@ -3,6 +3,7 @@ package com.example.timetracker.ui.fragment.home.createTask
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.timetracker.App
+import com.example.timetracker.common.live.SingleLiveEvent
 import com.example.timetracker.ui.base.BaseViewModel
 
 class CreateTaskBottomSheetDialogViewModel: BaseViewModel() {
@@ -11,7 +12,21 @@ class CreateTaskBottomSheetDialogViewModel: BaseViewModel() {
     val taskDescription = MutableLiveData("")
     val taskDate = MutableLiveData("")
 
+    val event = SingleLiveEvent<CreateTaskEvent>()
+
     init {
         App.appComponent?.inject(this)
     }
+
+    fun onCreateTaskClick() {
+        event.value = CreateTaskEvent.ADD_TASK
+    }
+
+    fun onDateClick() {
+        event.value = CreateTaskEvent.DATE
+    }
+}
+
+enum class CreateTaskEvent {
+    ADD_TASK, DATE
 }
